c4a217acd71bdf69f33cf152041d3fbd6415bd0d
LZYLRRY <162039530+LZYLRRY@users.noreply.github.com>
2024-09-29 15:37:45 +0200

    Add bar, circle & text \(under construction\)

---
+++ frequency/script.js
@@ -0,0 +1,22 @@
+const width = 600;
+const height = 600;
+
+// Add svg element
+let svg = d3
+    .select('body')
+    .append('svg')
+    .attr('width', width)
+    .attr('height', height);
+
+d3
+    .select('textarea')
+    .on('input', e => {
+        let frequencies = {};
+
+        e.target.value.split('').forEach(char => {
+            let currentCount = frequencies[char] || 0;
+            frequencies[char] = currentCount + 1;
+        });
+
+        console.log(frequencies);
+    });
